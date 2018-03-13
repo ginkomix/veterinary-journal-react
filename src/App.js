16fa5d91a7702f9d211fe926b42228549ae62f3a
ginkomix <accer96226@gmail.com>
2018-03-13 23:43:44 +0300

    firebase add

--- src/App.js
+++ src/App.js
@@ -9,6 +9,18 @@ import 'semantic-ui-css/semantic.min.css';
 import {Provider} from 'react-redux';
 import store from './config/store';
 import Start from './components/Start';
+import * as firebase from 'firebase';
+
+ var config = {
+    apiKey: "AIzaSyBJqf0gLujy-LORi1oFG41JE040bx1F0RE",
+    authDomain: "veterinary-333.firebaseapp.com",
+    databaseURL: "https://veterinary-333.firebaseio.com",
+    projectId: "veterinary-333",
+    storageBucket: "veterinary-333.appspot.com",
+    messagingSenderId: "509184734732"
+  };
+
+  firebase.initializeApp(config);
 
 
 class App extends React.Component {
