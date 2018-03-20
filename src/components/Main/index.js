83de325f8ec2a749fe0dc15c79b592507a1158be
ginkomix <accer96226@gmail.com>
2018-03-20 23:11:21 +0300

    верстка комнаты

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -10,12 +10,43 @@ import Filter from "../Filter";
 import {api} from "../../utils/api";
 import ContextMenu from "../ContextMenu";
 import './index.css';
+import ava from '../../images/ava.png';
+import { Icon } from 'semantic-ui-react';
 
 class Main extends React.Component {
 	render() {	
 		return(
 			<div className='main'>
-				
+				<div className='headerMain'>
+                <div className='headerMain-top'>
+				    <div className="user-profile">
+				        <img src={ava}  />
+				         <p className='fimale'>GINKO</p>
+				    <p className='name'>MCHAIL DMITRIEVITC</p>
+				    </div>
+				   <div className="button-control button-control-active " >
+                   <Icon name='plus'  />
+				       <p>ДОБАВИТЬ</p>
+				   </div>
+				    <div className="button-control">
+                   <Icon name='idea'  />
+				       <p>ИЗМЕНИТЬ</p>
+				   </div>
+				    <div className="button-control">
+                   <Icon name='delete'  />
+				       <p>УДАЛИТЬ</p>
+				   </div>
+				    </div>
+				    <div className="footer">
+                        <span>ВЫЙТИ</span>
+				        <p>GINKOMIX 2018</p>
+				    </div>
+				</div>
+				<div className='journal'>
+				    <div className="journal-box">
+				        
+				    </div>
+				</div>
 			</div>
 		)
 	}
