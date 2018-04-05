25146fa5131af79eebf9be421d17c4d981f22984
ginkomix <accer96226@gmail.com>
2018-04-05 22:10:44 +0300

    middleware add for promise

--- src/config/store.js
+++ src/config/store.js
@@ -1,5 +1,6 @@
-import { createStore } from "redux";
+import { createStore,applyMiddleware  } from "redux";
 import reducer from "../reducers";
+import { dispath } from '../middleware/dispath'
 
-const store = createStore(reducer);
+const store = createStore(reducer,applyMiddleware(dispath));
 export default store;
---
+++ src/middleware/dispath.js
@@ -0,0 +1,7 @@
+export const dispath = store => next => action => {
+ if (typeof action.then !== 'function') {
+    return next(action)
+  }
+  return Promise.resolve(action).then(store.dispatch)
+
+}
